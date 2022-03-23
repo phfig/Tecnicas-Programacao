994c1d28036008866ee95ff47298e6e103bca3d3
Pedro Figueiredo <80103780+phfig@users.noreply.github.com>
2022-03-22 21:41:43 -0300

    Exercícios Técnicas de Programação 22/03/22

---
+++ Aula 22_03/aula.js
@@ -0,0 +1,41 @@
+/*console.log("Técnicas de Programação");
+var idade;
+idade = 12;
+var cpf = ""
+var num = 6;
+var nome_do_funcionario;
+var NomeFuncionario = "Vanderson";String
+var nome = 'V'; caracter
+if(idade>18 && cpf !=""){
+    Bloco Verdade
+}
+//Operadores Lógicos
+&& = E
+|| = OU
+!= negação
+
+//condição simples
+if (num < 10) {
+    console.log("É menor");
+}
+//condição composta
+if (idade > 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}*/
+//Estruturas de Repetição
+var x = 3;
+while (x < 10) {
+    console.log("é menor");
+    x = x + 1;
+}
+console.log("Acabou!");
+
+//estrutura For
+for (var i = 0;i < 10;i++) {
+    console.log("O valor de i é: " + i);
+    
+}
+//variavel = variavel + 1;
+//variavel++;
---
+++ Aula 22_03/exercicios.js
@@ -0,0 +1,27 @@
+//Exercício 1
+var n1 = 5;
+var n2 = 9;
+var n3 = 10;
+
+var media = (n1 + n2 + n3) / 3;
+console.log("A média é: " + media);
+
+//Exercício 2
+var x = 10;
+var y = 30;
+if(x > y) {
+    console.log(x + "É maior");
+}else{
+    console.log(y + "É maior");
+}
+
+//Exercício 3
+
+var x = 5;
+for (var i = 0;i < 11;i++) {
+    var resultado = i*x;
+    console.log(i + " x " + x + " = " + resultado);
+
+}
+
+//Exercício 4
