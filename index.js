24839c0568f41bd3e8bf1b353ed50e911c72057e
Pedro Figueiredo <80103780+phfig@users.noreply.github.com>
2022-05-03 21:39:00 -0300

    Atualização 03/05/2022

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.log("Projeto Node");
-console.log("PEDRO MINECRAFT XD");
+/*const{createServer}=require('http');
+let server = createServer((request,response)=>{
+    response.writeHead(200,{"Content_Type":"text/html"});
+    response.write('<h1>Olá mundo</h1><p>Projeto</p>');
+    response.end();
+});
+server.listen(8000);
+console.log("Projeto Iniciado");*/
+let express = require('express');
+let app = express();
+
+app.get('/',function(req,res){
+    res.send('ROTA GET')
+});
+
+app.get('/apresentar',function(req,res){
+    //res.send('rota 1');
+    let param2 = req.query.n1;
+    let param3 = req.query.n2;
+    let soma = parseInt(param2) + parseInt(param3);
+    res.send("Resultado: " + soma);
+});
+
+app.listen(3000,function(){
+    console.log("Projeto iniciado na porta 3000")
+});
